03b35c68bd6a3b261083582b278200e19c25f9a4
Dualisc <59335457+Dualisc@users.noreply.github.com>
2020-06-09 09:29:32 +0200

    Add files via upload

---
+++ commons.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <windows.h>
+#include <TlHelp32.h>
+#include <vector>
+#include <d3dx9math.h>
+#pragma comment(lib, "d3dx9.lib")
+#include <algorithm>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <ctime>
+#include <map>
+#include <Psapi.h>
+#include <urlmon.h>
+#include <iomanip> 
+#include <wininet.h>
+#include <codecvt>
+#include <cstdint>
+#include <stdio.h>
+#include <fstream>
+#include <emmintrin.h>
